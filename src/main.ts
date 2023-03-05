5db076f6307e3389cc4009915f38ff581c58e03a
OldPanda <me@old-panda.com>
2023-03-04 21:55:03 -0800

    Try another way to add vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from '@/App.vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from '@/App.vue';
+import { inject } from '@vercel/analytics';
 
-import '@/style.css'
+import '@/style.css';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
+app.use(createPinia());
 
-app.mount('#app')
+app.mount('#app');
+
+inject();
