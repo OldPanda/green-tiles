5fd818d17e875fedeaa64cf25c5c924cd25ebf02
OldPanda <me@old-panda.com>
2023-03-04 16:28:31 -0800

    Add dark mode (#13)

--- src/components/Contributions.vue
+++ src/components/Contributions.vue
@@ -20,20 +20,22 @@
           <g transform="translate(15, 20)">
             <g v-for="(week, idx) in calendar.weeks" :transform="`translate(` + idx * 14 + `, 0)`">
               <rect width="10" height="10" v-for="day in week.days" :x="25 - idx" :y="day.weekday * 13" rx="2" ry="2"
-                :style="{ 'fill': fillColor(day.level) }">
+                :class="day.level">
                 <title>{{ tooltipStatement(day) }}</title>
               </rect>
             </g>
 
-            <text v-for="pair in monthPositions(calendar)" :x="pair.xPos" y="-7">{{ pair.month }}</text>
+            <text v-for=" pair in monthPositions(calendar)" :x="pair.xPos" y="-7" class="fill-base-content">
+              {{ pair.month }}
+            </text>
 
-            <text dx="-15" dy="8" style="display: none;">Sun</text>
-            <text dx="-15" dy="22">Mon</text>
-            <text dx="-15" dy="32" style="display: none;">Tue</text>
-            <text dx="-15" dy="48">Wed</text>
-            <text dx="-15" dy="57" style="display: none;">Thu</text>
-            <text dx="-15" dy="73">Fri</text>
-            <text dx="-15" dy="81" style="display: none;">Sat</text>
+            <text dx="-15" dy="8" style="display: none;" class="fill-base-content">Sun</text>
+            <text dx="-15" dy="22" class="fill-base-content">Mon</text>
+            <text dx="-15" dy="32" style="display: none;" class="fill-base-content">Tue</text>
+            <text dx="-15" dy="48" class="fill-base-content">Wed</text>
+            <text dx="-15" dy="57" style="display: none;" class="fill-base-content">Thu</text>
+            <text dx="-15" dy="73" class="fill-base-content">Fri</text>
+            <text dx="-15" dy="81" style="display: none;" class="fill-base-content">Sat</text>
           </g>
         </svg>
       </div>
@@ -43,27 +45,10 @@
 
 <script setup lang="ts">
 import { contributionsStore } from "@/stores/contributions";
-import { GITHUB_PROFILE_PREFIX, DEFAULT_COLORS, TRANSPARENT, MONTHS } from "@/constants";
+import { GITHUB_PROFILE_PREFIX, MONTHS } from "@/constants";
 import type { GitHubCalendar, GitHubDay, MonthPosPair } from "@/types";
 
 let contributions = contributionsStore();
-
-function fillColor(level: string): string {
-  switch (level) {
-    case "NONE":
-      return DEFAULT_COLORS[0];
-    case "FIRST_QUARTILE":
-      return DEFAULT_COLORS[1];
-    case "SECOND_QUARTILE":
-      return DEFAULT_COLORS[2];
-    case "THIRD_QUARTILE":
-      return DEFAULT_COLORS[3];
-    case "FOURTH_QUARTILE":
-      return DEFAULT_COLORS[4];
-    default:
-      return TRANSPARENT;
-  }
-}
 
 function monthPositions(calendar: GitHubCalendar): MonthPosPair[] {
   let pairs: MonthPosPair[] = [];
@@ -102,3 +87,25 @@ function tooltipStatement(day: GitHubDay): string {
   }
 }
 </script>
+
+<style scoped>
+.NONE {
+  fill: var(--none);
+}
+
+.FIRST_QUARTILE {
+  fill: var(--first);
+}
+
+.SECOND_QUARTILE {
+  fill: var(--second);
+}
+
+.THIRD_QUARTILE {
+  fill: var(--third);
+}
+
+.FOURTH_QUARTILE {
+  fill: var(--fourth);
+}
+</style>
